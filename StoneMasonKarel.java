cdd0b90ddcea74d27c6055d666e1108b1fd67c29
Zach Becker <doc.misterio@gmail.com>
2013-03-31 11:24:30 -0500

    redo first commit

---
+++ StoneMasonKarel.java
@@ -0,0 +1,41 @@
+/*
+ * File: StoneMasonKarel.java
+ * --------------------------
+ * The StoneMasonKarel subclass as it appears here does nothing.
+ * When you finish writing it, it should solve the "repair the quad"
+ * problem from Assignment 1.  In addition to editing the program,
+ * you should be sure to edit this comment so that it no longer
+ * indicates that the program does nothing.
+ */
+
+import stanford.karel.*;
+
+public class StoneMasonKarel extends SuperKarel {
+
+	public void run() {
+		/*for (int i = 0; i < 4; i++){
+			move();
+		} */
+		turnLeft();
+		while (frontIsClear()) {
+			if (beepersPresent()) {
+				move();
+			}
+			else {
+				putBeeper();
+			}
+			if (frontIsBlocked()) {
+				if (noBeepersPresent()){
+					putBeeper();
+				}
+			}
+		}
+		 
+		turnAround();
+		while (frontIsClear()) {
+			move();
+		}
+		turnLeft();
+	}
+
+}
